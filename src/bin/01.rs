b45acdb7c11f1f869e4067f0127b0e5cdb985f3c
Rohan Godha <git@rohangodha.com>
2025-12-01 00:12:07 -0500

    aoc day 1!!

---
+++ src/bin/01.rs
@@ -0,0 +1,65 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let mut dial = 50i64;
+    let mut cnt = 0;
+    for line in input.lines() {
+        dbg!(line);
+        if line.chars().next().unwrap() == 'R' {
+            let amt: i64 = line[1..].parse().unwrap();
+            dial = (dial + amt).rem_euclid(100);
+        } else {
+            let amt: i64 = line[1..].parse().unwrap();
+            dial = (dial - amt).rem_euclid(100);
+        }
+        dbg!(dial);
+        if dial == 0 {
+            cnt += 1;
+        }
+    }
+    Some(cnt)
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut dial = 50i64;
+    let mut cnt = 0;
+    for line in input.lines() {
+        let prev = dial;
+        let new;
+        dbg!((line, dial, cnt));
+        if line.chars().next().unwrap() == 'R' {
+            let amt: i64 = line[1..].parse().unwrap();
+
+            new = prev + amt;
+        } else {
+            let amt: i64 = line[1..].parse().unwrap();
+            new = prev - amt;
+        }
+        dbg!((new, prev));
+        if new.signum() != prev.signum() && prev != 0 {
+            println!("adding one bc sign change");
+            cnt += 1;
+        }
+        cnt += new.abs() / 100;
+        println!("adding {} ", new.abs() / 100);
+        dial = new.rem_euclid(100);
+    }
+    Some(cnt as usize)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(3));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(6));
+    }
+}
